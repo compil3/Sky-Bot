a073805e9f5458536cc56dc7da059e0331c63d6f
Luca <luca.tenuta@gmail.com>
2020-03-13 16:46:06 -0400

    added player properties

---
+++ Properties/PlayerProperties.cs
@@ -0,0 +1,100 @@
+﻿namespace Sky_Bot.Properties
+{
+    class PlayerProperties
+    {
+        public class StatProperties
+        {
+            public int SeasonId { get; set; }
+            public int Id { get; set; }
+            public string SeasonTypeId { get; set; }
+
+            public string PlayerSystem { get; set; }
+            public string Position { get; set; }
+            public string PlayerName { get; set; }
+            public string GamesPlayed { get; set; }
+            public string Record { get; set; }
+            public string AvgMatchRating { get; set; }
+            public string Goals { get; set; }
+            public string Assists { get; set; }
+            public string CleanSheets { get; set; }
+            public string ShotsOnGoal { get; set; }
+            public string ShotsOnTarget { get; set; }
+            public string ShotPercentage { get; set; }
+            public string Tackles { get; set; }
+            public string TackleAttempts { get; set; }
+            public string TacklePercentage { get; set; }
+            public string PassingPercentage { get; set; }
+            public string KeyPasses { get; set; }
+            public string Interceptions { get; set; }
+            public string Blocks { get; set; }
+            public string YellowCards { get; set; }
+            public string RedCards { get; set; }
+            public string ManOfTheMatch { get; set; }
+            public string PlayerUrl { get; set; }
+            public string TeamIcon { get; set; }
+        }
+
+        public class GoalieProperties
+        {
+            public int SeasonId { get; set; }
+            public int Id { get; set; }
+            public string SeasonTypeId { get; set; }
+
+            public string userSystem { get; set; }
+            public string playerName { get; set; }
+            public string lgRank { get; set; }
+            public string gamesPlayed { get; set; }
+            public string record { get; set; }
+            public string goalsAgainst { get; set; }
+            public string shotsAgainst { get; set; }
+            public string saves { get; set; }
+            public string savePercentage { get; set; }
+            public string goalsAgainstAvg { get; set; }
+            public string cleanSheets { get; set; }
+            public string manOfTheMatch { get; set; }
+            public string avgMatchRating { get; set; }
+            public string playerURL { get; set; }
+            public string teamIcon { get; set; }
+        }
+
+        public class PlayerInfo
+        {
+            public int Id { get; set; }
+            public string playerName { get; set; }
+            public string playerUrl { get; set; }
+        }
+
+        public class Career
+        {
+            public int Id { get; set; }
+            public string PlayerName { get; set; }
+
+            public string Record { get; set; }
+            public double GamesPlayed { get; set; }
+            public double AvgMatchRating { get; set; }
+
+            public double Goals { get; set; }
+            public string Assists { get; set; }
+
+            public double ShotAttempts { get; set; }
+            public string ShotsOnTarget { get; set; }
+            public double ShotPercentage { get; set; }
+
+            public double PassesCompleted { get; set; }
+            public double PassesAttempted { get; set; }
+            public double PassingPercentage { get; set; }
+            public string KeyPasses { get; set; }
+
+            public double Tackles { get; set; }
+            public double TackleAttempts { get; set; }
+            public double TacklePercentage { get; set; }
+
+            public string Interceptions { get; set; }
+            public string Blocks { get; set; }
+            public string YellowCards { get; set; }
+            public string RedCards { get; set; }
+        }
+
+    }
+    }
+}
