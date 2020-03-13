6d0ab7d2e43b70e61db87c45a16d89d614a8ca06
Luca <luca.tenuta@gmail.com>
2020-03-13 16:46:15 -0400

    added team properties

---
+++ Properties/TeamProperties.cs
@@ -0,0 +1,28 @@
+﻿namespace Sky_Bot.Properties
+{
+    class TeamProperties
+    {
+        public int SeasonId { get; set; }
+        public int Id { get; set; }
+        public string SeasonTypeId { get; set; }
+        public string System { get; set; }
+
+        public int Rank { get; set; }
+        public string TeamName { get; set; }
+        public string GamesPlayed { get; set; }
+        public string GamesWon { get; set; }
+        public string GamesDrawn { get; set; }
+        public string GamesLost { get; set; }
+        public string Points { get; set; }
+        public string Streak { get; set; }
+        public string GoalsFor { get; set; }
+        public string GoalsAgainst { get; set; }
+        public string CleanSheets { get; set; }
+        public string LastTenGames { get; set; }
+        public string HomeRecord { get; set; }
+        public string AwayRecord { get; set; }
+        public string OneGoalGames { get; set; }
+        public string TeamIconUrl { get; set; }
+        public string TeamURL { get; set; }
+    }
+}
