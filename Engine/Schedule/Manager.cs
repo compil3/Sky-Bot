cd5a5c1a9ef6df5bf5ce81fa1244d6ce6a9cf8a1
Luca <luca.tenuta@gmail.com>
2020-05-01 02:15:38 -0400

    Added a schedule manager.

---
+++ Engine/Schedule/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using FluentScheduler;
+
+
+namespace Engine.Schedule
+{
+    class Manager
+    {
+        public static Task Manage(IMessageChannel chnl)
+        {
+            JobManager.Initialize(new WeekUpdate(chnl));
+            return Task.CompletedTask;
+        }
+    }
+}
