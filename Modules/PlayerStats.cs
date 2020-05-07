c4847efe5a306bb65f5964626356c8acc6cadc02
Luca <luca.tenuta@gmail.com>
2020-05-07 01:44:51 -0400

    Changed the way Playerstats works.

--- Modules/PlayerStats.cs
+++ Modules/PlayerStats.cs
@@ -26,7 +26,7 @@ namespace Sky_Bot.Modules
             var outPutSeason = "";
             var guildID = Context.Guild.Id;
 
-            if (guildID == 174342051318464512)
+            if (guildID == 689119429375819951)
             {
                 Log.Logger.Warning($"{Context.Guild.Name} (LG command triggered)");
                 await Context.Channel.SendMessageAsync(null, embed: Player.GetPlayer(playerLookup, seasonType, seasonType)).ConfigureAwait(false);
@@ -38,118 +38,118 @@ namespace Sky_Bot.Modules
 
     }
 
-    internal class DisplayBuilder : ModuleBase
-    {
-        internal static Embed EmbedMessage(string playerLookup, string dbName, string seasonID, ulong guild)
-        {
-            #region variables
-            var success = false;
-            var tableName = "";
-            var outPutSeason = "";
-            var systemIcon = "";
-            var shotRecord = "";
-            var goalRecord = "";
-            var cleanMan = "";
-            var gaa = "";
-            var division = "";
-            EmbedBuilder builder;
-            Embed message = null;
-            #endregion
+    //internal class DisplayBuilder : ModuleBase
+    //{
+    //    internal static Embed EmbedMessage(string playerLookup, string dbName, string seasonID, ulong guild)
+    //    {
+    //        #region variables
+    //        var success = false;
+    //        var tableName = "";
+    //        var outPutSeason = "";
+    //        var systemIcon = "";
+    //        var shotRecord = "";
+    //        var goalRecord = "";
+    //        var cleanMan = "";
+    //        var gaa = "";
+    //        var division = "";
+    //        EmbedBuilder builder;
+    //        Embed message = null;
+    //        #endregion
 
-            try
-            {
-                using var playerDatabase = new LiteDatabase(dbName);
-                if (seasonID == null)
-                {
-                    tableName = "CurrentSeason";
-                    outPutSeason = "Regular Season";
-                }
+    //        try
+    //        {
+    //            using var playerDatabase = new LiteDatabase(dbName);
+    //            if (seasonID == null)
+    //            {
+    //                tableName = "CurrentSeason";
+    //                outPutSeason = "Regular Season";
+    //            }
 
-                var player = playerDatabase.GetCollection<PlayerProperties.StatProperties>(tableName);
-                var result = player.Find(x =>
-                    x.PlayerName.StartsWith(playerLookup) || x.PlayerName.ToLower().StartsWith(playerLookup));
+    //            var player = playerDatabase.GetCollection<PlayerProperties.StatProperties>(tableName);
+    //            var result = player.Find(x =>
+    //                x.PlayerName.StartsWith(playerLookup) || x.PlayerName.ToLower().StartsWith(playerLookup));
 
-                foreach (var found in result)
-                {
-                    if (guild == 174342051318464512)
-                    {
-                        division = string.Empty;
-                        if (found.Position == "G")
-                        {
-                            string[] shotsFaced = new string[3];
-                            shotsFaced[0] = found.ShotsAgainst;
-                            shotsFaced[1] = found.Saves;
-                            shotsFaced[2] = found.SavePercentage + "%";
-                            shotRecord = string.Join("-", shotsFaced);
+    //            foreach (var found in result)
+    //            {
+    //                if (guild == 174342051318464512)
+    //                {
+    //                    division = string.Empty;
+    //                    if (found.Position == "G")
+    //                    {
+    //                        string[] shotsFaced = new string[3];
+    //                        shotsFaced[0] = found.ShotsAgainst;
+    //                        shotsFaced[1] = found.Saves;
+    //                        shotsFaced[2] = found.SavePercentage + "%";
+    //                        shotRecord = string.Join("-", shotsFaced);
 
-                            string[] goalsAllowed = new string[2];
-                            goalsAllowed[0] = found.GoalsAgainst;
-                            goalsAllowed[1] = found.GoalsAgainstAvg;
-                            goalRecord = string.Join(" - ", goalsAllowed);
+    //                        string[] goalsAllowed = new string[2];
+    //                        goalsAllowed[0] = found.GoalsAgainst;
+    //                        goalsAllowed[1] = found.GoalsAgainstAvg;
+    //                        goalRecord = string.Join(" - ", goalsAllowed);
 
-                            string[] goalieCm = new string[2];
-                            goalieCm[0] = found.CleanSheets;
-                            goalieCm[1] = found.ManOfTheMatch;
-                            cleanMan = string.Join("-", goalieCm);
+    //                        string[] goalieCm = new string[2];
+    //                        goalieCm[0] = found.CleanSheets;
+    //                        goalieCm[1] = found.ManOfTheMatch;
+    //                        cleanMan = string.Join("-", goalieCm);
 
-                            if (found.UserSystem == "psn")
-                            {
-                                systemIcon =
-                                    "https://media.playstation.com/is/image/SCEA/navigation_home_ps-logo-us?$Icon$";
-                            }
-                            else if (found.UserSystem == "xbox")
-                            {
-                                systemIcon =
-                                    "http://www.logospng.com/images/171/black-xbox-icon-171624.png";
-                            }
+    //                        if (found.UserSystem == "psn")
+    //                        {
+    //                            systemIcon =
+    //                                "https://media.playstation.com/is/image/SCEA/navigation_home_ps-logo-us?$Icon$";
+    //                        }
+    //                        else if (found.UserSystem == "xbox")
+    //                        {
+    //                            systemIcon =
+    //                                "http://www.logospng.com/images/171/black-xbox-icon-171624.png";
+    //                        }
 
-                            message = Helpers.BuilderLG(found.UserSystem, shotRecord, goalRecord, cleanMan, systemIcon,
-                                found.SeasonId, found.PlayerName, found.Position, found.PlayerUrl, found.TeamIcon,
-                                found.Record, found.AvgMatchRating, guild, division);
-                        }
-                        else if (found.Position != "G")
-                        {
+    //                        message = Helpers.BuilderLG(found.UserSystem, shotRecord, goalRecord, cleanMan, systemIcon,
+    //                            found.SeasonId, found.PlayerName, found.Position, found.PlayerUrl, found.TeamIcon,
+    //                            found.Record, found.AvgMatchRating, guild, division);
+    //                    }
+    //                    else if (found.Position != "G")
+    //                    {
 
-                        }
-                    }
-                    else if (guild == 689119429375819951)
-                    {
-                        if (found.Position == "G")
-                        {
-                            string[] shotsFaced = new string[3];
-                            shotsFaced[0] = found.GoalsAgainst;
-                            shotsFaced[1] = found.Saves;
-                            shotsFaced[2] = found.SavePercentage;
-                            shotRecord = string.Join("-", shotsFaced);
+    //                    }
+    //                }
+    //                else if (guild == 689119429375819951)
+    //                {
+    //                    if (found.Position == "G")
+    //                    {
+    //                        string[] shotsFaced = new string[3];
+    //                        shotsFaced[0] = found.GoalsAgainst;
+    //                        shotsFaced[1] = found.Saves;
+    //                        shotsFaced[2] = found.SavePercentage;
+    //                        shotRecord = string.Join("-", shotsFaced);
 
-                            string[] goalsAgainstAvg = new string[2];
-                            goalsAgainstAvg[0] = found.GoalsAgainst;
-                            goalsAgainstAvg[1] = found.GoalsAgainstAvg;
-                            gaa = string.Join("-", goalsAgainstAvg);
+    //                        string[] goalsAgainstAvg = new string[2];
+    //                        goalsAgainstAvg[0] = found.GoalsAgainst;
+    //                        goalsAgainstAvg[1] = found.GoalsAgainstAvg;
+    //                        gaa = string.Join("-", goalsAgainstAvg);
 
-                            string[] goalieCleanMan = new string[2];
-                            goalieCleanMan[0] = found.CleanSheets;
-                            goalieCleanMan[1] = found.ManOfTheMatch;
-                            cleanMan = string.Join("-", goalieCleanMan);
-                            message = Helpers.BuilderPCN(found.UserSystem, shotRecord, goalRecord, cleanMan, systemIcon,
-                                found.SeasonId, found.PlayerName, found.Position, found.PlayerUrl, found.TeamIcon,
-                                found.Record, found.AvgMatchRating, guild, division);
+    //                        string[] goalieCleanMan = new string[2];
+    //                        goalieCleanMan[0] = found.CleanSheets;
+    //                        goalieCleanMan[1] = found.ManOfTheMatch;
+    //                        cleanMan = string.Join("-", goalieCleanMan);
+    //                        message = Helpers.BuilderPCN(found.UserSystem, shotRecord, goalRecord, cleanMan, systemIcon,
+    //                            found.SeasonId, found.PlayerName, found.Position, found.PlayerUrl, found.TeamIcon,
+    //                            found.Record, found.AvgMatchRating, guild, division);
 
-                        } else if (found.Position != "G")
-                        {
+    //                    } else if (found.Position != "G")
+    //                    {
 
-                        }
+    //                    }
                        
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
+    //                }
+    //            }
+    //        }
+    //        catch (Exception e)
+    //        {
+    //            Console.WriteLine(e);
+    //            throw;
+    //        }
 
-            return message;
-        }
-    }
+    //        return message;
+    //    }
+    //}
 }
