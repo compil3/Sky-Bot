e511d973994b929d729338544924d1c517ef2017
Luca <luca.tenuta@gmail.com>
2020-05-12 02:22:47 -0400

    Cleaned up code from Xpath to LINQ/Lambda expression.

--- Engines/CareerSeason.cs
+++ Engines/CareerSeason.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using HtmlAgilityPack;
 using LiteDB;
+using Microsoft.VisualBasic;
+using Serilog;
 using Sky_Bot.Modules;
 using Sky_Bot.Properties;
 
@@ -14,6 +18,8 @@ namespace Sky_Bot.Engines
 {
     public class CareerSeason
     {
+        public object SeasonId { get; private set; }
+
         public static Embed CareerSeasonEmbed(HtmlDocument playerDoc, string foundPlayerUrl, string foundPlayerName, string seasonId, int leagueId)
         {
             HtmlNodeCollection findCareerNode = null;
@@ -24,137 +30,80 @@ namespace Sky_Bot.Engines
             var web = new HtmlWeb();
             try
             {
-                using (var playerDatabase = new LiteDatabase(@"Filename=Database/LGFA.db;connection=shared"))
+
+                var playerVirutalCareer = new HtmlDocument();
+
+
+
+                findCareerNode =
+                    playerDoc.DocumentNode.SelectNodes(
+                        $"//*[@id='lg_team_user_leagues-{leagueId}']/div[3]/table/tbody/tr");
+
+                if (findCareerNode == null)
                 {
-                    var player = playerDatabase.GetCollection<PlayerProperties.PlayerInfo>("Players");
-                    player.EnsureIndex(x => x.playerName);
-
-                    var result = player.Query()
-                        .Where(x => x.playerName.Contains(foundPlayerName))
-                        .ToList();
-                    var playerVirutalCareer = new HtmlDocument();
-
-                    foreach (var found in result)
-                    {
-                        playerVirutalCareer = web.Load(found.playerUrl);
-                    }
-                    findCareerNode =
-                         playerDoc.DocumentNode.SelectNodes($"//*[@id='lg_team_user_leagues-{leagueId}']/div[3]/table/tbody/tr");
-
-                    if (findCareerNode == null)
-                    {
-                        findCareerNode = playerDoc.DocumentNode.SelectNodes(
-                            $"//*[@id='lg_team_user_leagues-{leagueId}']/div[4]/table/tbody/tr");
-                        div = 4;
-                    }
-                    else div = 3;
-                    var index = 1;
-
-
-                    //this works to collect all the rows, but it's multidimensional.
-                    //need to add Season = "S16", Type = "Reg", GP = "20" etc
-                    List<List<string>> table = playerDoc.DocumentNode
-                        .SelectSingleNode($"//*[@id='lg_team_user_leagues-{leagueId}']/div[4]/table/tbody")
-                        .Descendants("tr")
-                        .Skip(1)
-                        //.Where(tr => tr.Elements("td").Count() > 1)
-                        .Select(tr => tr.Elements("td")
-                            .Select(td => td.InnerText.Trim()).ToList())
-                        .ToList();
-
-
-                    var tableResult = Enumerable.Range(0, table.Count)
-                        .Where(i => table[i].Contains(seasonId) && table[i].Contains("Reg"))
-                        .ToString();
-
-              var count = findCareerNode.Count;
-                    foreach (var careerStats in findCareerNode)
-                    {
-                        if (WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode($"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[1]").InnerText).Trim() != seasonId)
-                        {
-                            index++;
-                            continue;
-                        }else if (WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode(
-                                $"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[2]")
-                            .InnerText).Trim() != "Reg")
-                        {
-                            index++;
-                            continue;
-                        }
-                        var type = WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode(
-                                $"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[2]")
-                            .InnerText);
-                        var record = WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode(
-                                $"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[3]")
-                            .InnerText);
-                        var amr = WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode(
-                                $"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[4]")
-                            .InnerText) ?? "0.0";
-                        var goals = WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode(
-                                $"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[5]")
-                            .InnerText);
-                        var assists = WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode(
-                                $"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[6]")
-                            .InnerText);
-                        var sot = WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode(
-                                $"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[7]")
-                            .InnerText);
-                        var shots = WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode(
-                                $"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[8]")
-                            .InnerText);
-                        var passC = WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode(
-                                $"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[9]")
-                            .InnerText);
-                        var passA = WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode(
-                                $"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[10]")
-                            .InnerText);
-                        var keypass = WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode(
-                                $"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[11]")
-                            .InnerText);
-                        var interceptions = WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode(
-                                $"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[12]")
-                            .InnerText);
-                        var tac = WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode(
-                                $"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[13]")
-                            .InnerText);
-                        var tacA = WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode(
-                                $"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[14]")
-                            .InnerText);
-                        var blk = WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode(
-                                $"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[15]")
-                            .InnerText);
-                        var rc = WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode(
-                                $"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[16]")
-                            .InnerText);
-                        var yc = WebUtility.HtmlDecode(careerStats
-                            .SelectSingleNode(
-                                $"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody/tr[{index}]/td[17]")
-                            .InnerText);
-                        GC.Collect();
-
-                        return EmbedHelpers.CareerEmbed(foundPlayerName, foundPlayerUrl, record, amr,
-                            goals, assists, sot, shots, passC,
-                            passA, keypass, interceptions, tac, tacA, blk, rc, yc, type, seasonId);
-                    }
+                    findCareerNode = playerDoc.DocumentNode.SelectNodes(
+                        $"//*[@id='lg_team_user_leagues-{leagueId}']/div[4]/table/tbody/tr");
+                    div = 4;
                 }
+                else div = 3;
 
+                var index = 1;
+
+
+                //this works to collect all the rows, but it's multidimensional.
+                //need to add Season = "S16", Type = "Reg", GP = "20" etc
+                var stopWatch = new Stopwatch();
+                stopWatch.Start();
+
+                // Now the return type is a List of CareerProperties.
+                List<CareerProperties> table = playerDoc.DocumentNode
+                    .SelectSingleNode($"//*[@id='lg_team_user_leagues-{leagueId}']/div[{div}]/table/tbody")
+                    .Descendants("tr")
+                    .Skip(0)
+                    // Up to here is your code. Here you select all rows from the table.
+                    // Each row is presented as List<string>.
+                    .Select(tr => tr.Elements("td").Select(td => td.InnerText.Trim()).ToList())
+                    // Here we filter table rows by "seasonId" and "Reg".
+                    .Where(tr => tr[0] == seasonId && tr[1] == "Reg")
+                    // Here we create objects CareerProperties from filtered rows.
+                    .Select(tr => new CareerProperties
+                    {
+                        Record = tr[2],
+                        MatchRating = tr[3],
+                        Goals = tr[4],
+                        Assists = tr[5],
+                        ShotsOnTarget = tr[6],
+                        ShotAttempts = tr[7],
+                        PassesCompleted = tr[8],
+                        PassesAttempted = tr[9],
+                        KeyPasses = tr[10],
+                        Interceptions = tr[11],
+                        Tackles = tr[12],
+                        TackleAttempts = tr[13],
+                        Blocks = tr[14],
+                        RedCards = tr[15],
+                        YellowCards = tr[16]
+
+                    })
+                    .ToList();
+
+                stopWatch.Stop();
+                Log.Logger.Warning($"Elapsed: {stopWatch.Elapsed}");
+
+                var temp = "";
+
+                //foreach (var godDamn in data)
+                //{
+                //    Log.Logger.Warning(godDamn.Season);
+                //}
+                var count = findCareerNode.Count;
+                foreach (var careerStats in table)
+                {
+                    return EmbedHelpers.CareerEmbed(foundPlayerName, foundPlayerUrl, careerStats.Record, careerStats.AvgMatchRating,
+                        careerStats.Goals, careerStats.Assists, careerStats.ShotsOnTarget, careerStats.ShotAttempts, careerStats.PassesCompleted,
+                        careerStats.PassesAttempted, careerStats.KeyPasses, careerStats.Interceptions, careerStats.Tackles, careerStats.TackleAttempts,
+                        careerStats.Blocks, careerStats.RedCards, careerStats.YellowCards, "Reg", seasonId);
+                }
             }
             catch (Exception e)
             {
@@ -164,10 +113,6 @@ namespace Sky_Bot.Engines
 
         }
 
-        internal static void CareerSeasonEmbed(List<PlayerProperties.PlayerInfo> result)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
 
