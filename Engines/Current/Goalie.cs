c516377d9609ca49e202f37fabde10c1b2e71df2
compil3 <luca.tenuta@gmail.com>
2020-05-20 21:04:50 -0400

    moved files to new location

---
+++ Engines/Current/Goalie.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Text;
+using System.Web;
+using HtmlAgilityPack;
+using LGFA.Essentials;
+using LGFA.Essentials.Writer;
+using Serilog;
+
+namespace LGFA.Engines
+{
+    class Goalie
+    {
+        public static bool GetGoalieLG(string league, string trigger, int histSeasonID, string seasonTypeID,
+            string command)
+        {
+            var st = new StackTrace();
+            var sf = st.GetFrame(0);
+            var currentMethod = sf.GetMethod();
+
+            var thisFile = new System.Diagnostics.StackTrace(true).GetFrame(0).GetFileName();
+
+            var web = new HtmlWeb();
+            var playerStatsDoc = web.Load(RetrieveUrl.GetUrl(league, trigger, histSeasonID, seasonTypeID));
+
+            var countPlayers = playerStatsDoc.DocumentNode.SelectNodes(("//*[@id='lgtable_goaliestats51']/tbody/tr"));
+            if (countPlayers == null) return false;
+            var playerTotal = countPlayers.Count;
+
+            try
+            {
+                for (int i = 1; i < playerTotal; i++)
+                {
+                    var findPlayerNode =
+                        playerStatsDoc.DocumentNode.SelectNodes($"//*[@id='lgtable_goaliestats51']/tbody/tr[1]");
+                    if (findPlayerNode == null) break;
+
+                    foreach (var player in findPlayerNode)
+                    {
+                        #region parse variables
+
+                        var position = "";
+                        var lgRank = "";
+                        var teamIconShort = "";
+                        var playerName = "";
+                        var gamesPlayed = "";
+                        var record = "";
+                        var goalsAgainst = "";
+                        var shotsAgainst = "";
+                        var saves = "";
+                        var savePercentage = "";
+                        var goalsAgainstAvg = "";
+                        var passingPercentage = "";
+                        var cleanSheets = "";
+                        var manOfTheMatch = "";
+                        var avgMatchRating = "";
+                        var playerShortURL = "";
+
+                        #endregion
+
+                        position = WebUtility.HtmlDecode(player
+                            .SelectSingleNode($"//*[@id='lgtable_memberstats51']/tbody/tr[{i}]/td[2]/span").InnerText);
+                        teamIconShort = WebUtility.HtmlDecode(player
+                            .SelectSingleNode($"//*[@id='lgtable_goaliestats51']/tbody/tr[{i}]/td[2]/img")
+                            .Attributes["src"].Value);
+                        playerName = WebUtility.HtmlDecode(player
+                            .SelectSingleNode($"//*[@id='lgtable_goaliestats51']/tbody/tr[{i}]/td[2]/a").InnerText);
+                        gamesPlayed = WebUtility.HtmlDecode(player
+                            .SelectSingleNode($"//*[@id='lgtable_goaliestats51']/tbody/tr[{i}]/td[4]").InnerText);
+                        record = WebUtility.HtmlDecode(player
+                            .SelectSingleNode($"//*[@id='lgtable_goaliestats51']/tbody/tr[{i}]/td[5]").InnerText);
+                        goalsAgainst = WebUtility.HtmlDecode(player
+                            .SelectSingleNode($"//*[@id='lgtable_goaliestats51']/tbody/tr[{i}]/td[6]").InnerText);
+                        shotsAgainst = WebUtility.HtmlDecode(player
+                            .SelectSingleNode($"//*[@id='lgtable_goaliestats51']/tbody/tr[{i}]/td[7]").InnerText);
+                        saves = WebUtility.HtmlDecode(player
+                            .SelectSingleNode($"//*[@id='lgtable_goaliestats51']/tbody/tr[{i}]/td[8]").InnerText);
+                        savePercentage = WebUtility.HtmlDecode(player
+                            .SelectSingleNode($"//*[@id='lgtable_goaliestats51']/tbody/tr[{i}]/td[9]").InnerText);
+                        goalsAgainstAvg = WebUtility.HtmlDecode(player
+                            .SelectSingleNode($"//*[@id='lgtable_goaliestats51']/tbody/tr[{i}]/td[10]").InnerText);
+                        cleanSheets = WebUtility.HtmlDecode(player
+                            .SelectSingleNode($"//*[@id='lgtable_goaliestats51']/tbody/tr[{i}]/td[11]").InnerText);
+                        manOfTheMatch = WebUtility.HtmlDecode(player
+                            .SelectSingleNode($"//*[@id='lgtable_goaliestats51']/tbody/tr[{i}]/td[12]").InnerText);
+                        avgMatchRating = WebUtility.HtmlDecode(player
+                            .SelectSingleNode($"//*[@id='lgtable_goaliestats51']/tbody/tr[{i}]/td[13]").InnerText);
+                        playerShortURL = WebUtility.HtmlDecode(player
+                            .SelectSingleNode($"//*[@id='lgtable_goaliestats51']/tbody/tr[{i}]/td[2]/a")
+                            .Attributes["href"].Value);
+
+                        var playerURL = string.Join(string.Empty,
+                            "https://www.leaguegaming.com/forums/" + playerShortURL);
+                        var temp = HttpUtility.ParseQueryString(new Uri(playerURL).Query);
+                        var playerID = int.Parse(temp.Get("userid"));
+                        var iconEnlarge = teamIconShort.Replace("p16", "p100");
+                        var iconURL = string.Join(string.Empty, "https://www.leaguegaming.com" + iconEnlarge);
+
+                        if (seasonTypeID == "pre") seasonTypeID = "pre-season";
+                        else if (seasonTypeID == "reg") seasonTypeID = "regular";
+                        if (savePercentage == String.Empty) savePercentage = "0";
+                        SavePlayerInfo.SavePlayerUrl(playerID, playerName, playerURL);
+
+                        //CareerBuilder.GetCareer(playerID, playerName, league);
+                        LGWriter.SaveGoalie(playerID, league, playerName,
+                            gamesPlayed, record, goalsAgainst, shotsAgainst, saves, savePercentage, goalsAgainstAvg,
+                            cleanSheets, manOfTheMatch, avgMatchRating, playerURL, iconURL, command, histSeasonID,
+                            seasonTypeID);
+                        GC.Collect();
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e,$"Error processing Goalie stats {currentMethod}");
+                return false;
+            }
+        }
+    }
+}
---
+++ Engines/Current/Player.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using Discord;
+using HtmlAgilityPack;
+using LGFA.Modules;
+using LGFA.Modules.Helpers;
+using LGFA.Properties;
+using LiteDB;
+using Serilog;
+using Missing = LGFA.Modules.Helpers.Missing;
+
+namespace LGFA.Engines
+{
+    class Player
+    {
+        //public static bool GetPlayer(string lookUpName, string PcnOrLg, string trigger, int histSeasonID, string seasonTypeID,
+        //    string command, ProgressBar pbar)
+        public static Embed GetPlayer(string lookUpName, string seasonType, string seasonId)
+        {
+            Embed message = null;
+            var systemIcon = "";
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            try
+            {
+                var dbPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                var dbFolder = "Database/";
+                var dbDir = Path.Combine(dbPath, dbFolder);
+
+                using var playerDatabase = new LiteDatabase($@"Filename={dbDir}LGFA.db;connection=shared");
+                var player = playerDatabase.GetCollection<PlayerProperties.PlayerInfo>("Players");
+
+                player.EnsureIndex(x => x.playerName);
+                var result = player.Query()
+                    .Where(x => x.playerName.Contains(lookUpName))
+                    .ToList();
+
+                foreach (var found in result)
+                {
+                    var playerHtml = found.playerUrl;
+                    var playerSystem = found.System;
+                    var system = 0;
+                    var type = 0;
+                    var web = new HtmlWeb();
+
+                    //if (seasonType == "pre") type = 1;
+                    //else if (seasonType == "reg") type = 2;
+                    //else if (seasonType == "qual") type = 3;
+
+                    if (playerSystem == "xbox")system = 53;
+                    else if (playerSystem == "psn") system = 73;
+
+                    if (found.System == "psn")
+                        systemIcon =
+                            "https://cdn.discordapp.com/attachments/689119430021873737/711030693743820800/220px-PlayStation_logo.svg.jpg";//"https://www.leaguegaming.com/images/league/icon/l73.png";
+                    else if (found.System == "xbox") systemIcon =
+                        "https://cdn.discordapp.com/attachments/689119430021873737/711030386775293962/120px-Xbox_one_logo.svg.jpg";//systemIcon = "https://www.leaguegaming.com/images/league/icon/l53.png";
+
+
+                    var playerDoc = web.Load(playerHtml);
+                    var tableHeadinng =
+                        playerDoc.DocumentNode.SelectNodes($"//*[@id='lg_team_user_leagues-{system}']/h3[1]");
+                    foreach (var heading in tableHeadinng)
+                    {
+                        var lgfa = WebUtility.HtmlDecode(heading
+                            .SelectSingleNode($"//*[@id='lg_team_user_leagues-{system}']/h3[1]").InnerText);
+                        if (lgfa.Contains("Last") || lgfa.Contains("Season Stats") || lgfa.Contains("CareerBuilder")) return Missing.NotFound(lookUpName, systemIcon, playerHtml);
+                        if (lgfa.Contains("LGFA - Season") || lgfa.Contains("LGFA PSN - Season"))
+                        {
+                            var seasonNumber = EmbedHelpers.Splitter(lgfa);
+                            seasonId = seasonNumber.Replace(" ", "");
+                        }
+                    }
+                    try
+                    {
+                        HtmlNodeCollection findStatRow = null;
+                        //find all the season types in the table.
+                        if (seasonType == null)
+                        {
+                            if (playerDoc.DocumentNode.SelectNodes(
+                                    $"//*[@id='lg_team_user_leagues-{system}']/div[1]/table/tbody/tr[1]") != null &&
+                                playerDoc.DocumentNode.SelectNodes(
+                                    $"//*[@id='lg_team_user_leagues-{system}']/div[1]/table/tbody/tr[2]") == null &&
+                                playerDoc.DocumentNode.SelectNodes(
+                                    $"//*[@id='lg_team_user_leagues-{system}']/div[1]/table/tbody/tr[3]") == null)
+                            {
+                                findStatRow = playerDoc.DocumentNode.SelectNodes(
+                                    $"//*[@id='lg_team_user_leagues-{system}']/div[1]/table/tbody/tr[1]");
+                                type = 1;
+                                seasonId = $"{seasonId} Pre-Season";
+                            }
+                            else if (playerDoc.DocumentNode.SelectNodes(
+                                         $"//*[@id='lg_team_user_leagues-{system}']/div[1]/table/tbody/tr[1]") !=
+                                     null &&
+                                     playerDoc.DocumentNode.SelectNodes(
+                                         $"//*[@id='lg_team_user_leagues-{system}']/div[1]/table/tbody/tr[2]") !=
+                                     null &&
+                                     playerDoc.DocumentNode.SelectNodes(
+                                         $"//*[@id='lg_team_user_leagues-{system}']/div[1]/table/tbody/tr[3]") ==
+                                     null)
+                            {
+                                findStatRow = playerDoc.DocumentNode.SelectNodes(
+                                    $"//*[@id='lg_team_user_leagues-{system}']/div[1]/table/tbody/tr[2]");
+                                type = 2;
+                                seasonId = $"{seasonId} Regular Season";
+                            }
+                            else if (playerDoc.DocumentNode.SelectNodes(
+                                         $"//*[@id='lg_team_user_leagues-{system}']/div[1]/table/tbody/tr[1]") !=
+                                     null &&
+                                     playerDoc.DocumentNode.SelectNodes(
+                                         $"//*[@id='lg_team_user_leagues-{system}']/div[1]/table/tbody/tr[2]") !=
+                                     null &&
+                                     playerDoc.DocumentNode.SelectNodes(
+                                         $"//*[@id='lg_team_user_leagues-{system}']/div[1]/table/tbody/tr[3]") !=
+                                     null)
+                            {
+                                findStatRow = playerDoc.DocumentNode.SelectNodes(
+                                    $"//*[@id='lg_team_user_leagues-{system}']/div[1]/table/tbody/tr[3]");
+                                type = 3;
+                                seasonId = $"{seasonId} Qualifier";
+                            }
+                        }
+                        else
+                        {
+                            findStatRow = playerDoc.DocumentNode.SelectNodes(
+                                $"//*[@id='lg_team_user_leagues-{system}']/div[1]/table/tbody/tr[{type}]");
+                        }
+                        if (findStatRow == null)
+                        {
+                            return message = Missing.NotFound(found.playerName, systemIcon, found.playerUrl);
+                        }
+                        
+                        var table = playerDoc.DocumentNode
+                            .SelectSingleNode($"//*[@id='lg_team_user_leagues-{system}']/div[1]/table/tbody")
+                            .Descendants("tr")
+                            .Skip(0)
+                            // Up to here is your code. Here you select all rows from the table.
+                            // Each row is presented as List<string>.
+                            .Select(tr => tr.Elements("td").Select(td => td.InnerText.Trim()).ToList())
+                            // Here we filter table rows by "seasonId" and "Reg".
+                            .Where(tr => tr[0] == "Reg") 
+                            // Here we create objects CareerProperties from filtered rows.
+                            .Select(tr => new SeasonProperties
+                            {
+                                Record = tr[1],
+                                AvgMatchRating = tr[2],
+                                Goals = Convert.ToDouble(tr[3]),
+                                Assists = tr[4],
+                                ShotsOnTarget = tr[5],
+                                ShotAttempts = tr[6],
+                                PassesCompleted = tr[7],
+                                PassesAttempted = tr[8],
+                                KeyPasses = tr[9],
+                                Interceptions = tr[10],
+                                Tackles = tr[11],
+                                TackleAttempts = tr[12],
+                                PossW = tr[13],
+                                PossL = tr[14],
+                                Blocks = tr[15],
+                                RedCards = tr[16],
+                                YellowCards = tr[17]
+
+                            })
+                            .ToList();
+
+
+                        var tempTeamIcon = "";
+                        var position = "";
+                        foreach (var teamFive in findStatRow)
+                        {
+                            tempTeamIcon = teamFive
+                                .SelectSingleNode(
+                                    $"//*[@id='content']/div/div/div[3]/div[1]/div/table/thead/tr/th/div/a/img")
+                                .Attributes["src"].Value;
+                            var lastFiveGames = playerDoc.DocumentNode.SelectNodes(
+                                $"//*[@id='lg_team_user_leagues-{system}']/div[2]/table/tbody/tr");
+                            foreach (var recent in lastFiveGames)
+                            {
+                                position = WebUtility.HtmlDecode(recent
+                                    .SelectSingleNode(
+                                        $"//*[@id='lg_team_user_leagues-{system}']/div[2]/table/tbody/tr[1]/td[2]")
+                                    .InnerText);
+                                break;
+                            }
+                        }
+
+                        foreach (var pStat in table)
+                        {
+                            var playerName = found.playerName;
+                            var teamIcon = string.Join(String.Empty, "http://www.leaguegaming.com" + tempTeamIcon);
+
+                            return SeasonHelper.SeasonEmbed(found.playerName, playerSystem, systemIcon, pStat.Record, pStat.AvgMatchRating,
+                                pStat.Goals.ToString(), pStat.Assists, pStat.ShotsOnTarget, pStat.ShotAttempts, pStat.PassesCompleted, pStat.PassesAttempted, pStat.KeyPasses,
+                                pStat.Interceptions, pStat.Tackles, pStat.TackleAttempts, pStat.PossW, pStat.PossL, pStat.Blocks, pStat.RedCards, pStat.YellowCards, seasonId, found.playerUrl,
+                                teamIcon, position);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Logger.Error($"Error processing stats. {e}");
+                        return null;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Logger.Error($"Error: {e}");
+                throw;
+            }
+            return null;
+        }
+    }
+}
