4c1b3b2c581547b9d1314d2dd530d2815f54fe0f
compil3 <luca.tenuta@gmail.com>
2020-05-26 03:05:33 -0400

    cleaned up and fixed player stat command

--- Modules/PlayerStats.cs
+++ Modules/PlayerStats.cs
@@ -6,6 +6,7 @@ using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using LGFA.Engines;
+using LGFA.Engines.Current.Player;
 using LGFA.Extensions;
 using LiteDB;
 using Serilog;
@@ -19,7 +20,7 @@ namespace LGFA.Modules
     {
         [Command("ps")]
         [Summary("Get the players current season statistics.")]
-        public async Task GetPlayerStatsLG(string playerLookup, string seasonType = null, string seasonId = null)
+        public async Task GetPlayerStatsLg(string playerLookup, string seasonType = null, string seasonId = null)
         {
             if (!Context.User.IsBot)
             {
@@ -34,11 +35,71 @@ namespace LGFA.Modules
                 //Log.Logger.Warning($"{Context.Guild.Name} (LG command triggered)");
                 //await Context.Channel.SendMessageAsync("``[Stats Provided by LGFA]``",
                 //    embed: Player.GetPlayer(playerLookup, seasonType, seasonId)).ConfigureAwait(false);
-                var test = LeagueInfo.GetSeason();
+                var player = CurrentSeason.SeasonStats(playerLookup);
+                var embed = SeasonEmbed(player);
+                await Context.Channel.SendMessageAsync("``[Stats Provided by LGFA]``", embed: embed)
+                    .ConfigureAwait(false);
             }
             else
                 await ReplyAsync(
                     $"Channel permission denied.  Try again in the proper channel {Context.User.Mention}");
+        }
+
+        private Embed SeasonEmbed(List<CareerProperties> player)
+        {
+            Embed embed = null;
+
+            foreach (var pStat in player)
+            {
+                var builder = new EmbedBuilder()
+                    .WithTitle($"{pStat.PlayerName} (Recent POS: {pStat.Position.Trim()})")
+                    .WithUrl(pStat.PlayerUrl)
+                    .WithColor(new Color(0x26A20B))
+                    .WithCurrentTimestamp()
+                    .WithFooter(footer =>
+                    {
+                        footer
+                            .WithText("leaguegaming.com")
+                            .WithIconUrl("https://www.leaguegaming.com/images/logo/logonew.png");
+                    })
+                    .WithAuthor(author =>
+                    {
+                        author
+                            .WithName($"{pStat.System.ToUpper()} {pStat.SeasonId} Stats")
+                            .WithIconUrl(pStat.SystemIcon);
+                    })
+                    .AddField("\u200B", $"```Record```", false)
+                    .AddField("GP", pStat.GamesPlayed, true)
+                    .AddField("Record (W-D-L)", pStat.Record, true)
+                    .AddField("AMR", pStat.MatchRating, true)
+
+                    .AddField("\u200B", $"```Offensive Stats```", false)
+                    .AddField("Goals", pStat.Goals, true)
+                    .AddField("G/Game", pStat.GoalsPerGame, true)
+                    .AddField("Shots - SOT", pStat.ShotSot, true)
+                    .AddField("S/Game", pStat.ShotPerGame, true)
+                    .AddField("Shots/Goal - SH%", pStat.ShotPerGoal, true)
+
+                    .AddField("\u200B", $"```Passing Stats```", false)
+                    .AddField("Assists", pStat.Assists, true)
+                    .AddField("Pass - Pass Attempts", pStat.PassRecord, true)
+                    .AddField("Key Passes", pStat.KeyPasses, true)
+                    .AddField("AST/Game", pStat.AssistPerGame, true)
+                    .AddField("P/Game - Pass %", pStat.PassPerGame, true)
+                    .AddField("Key Pass/Game", pStat.KeyPassPerGame, true)
+
+                    .AddField("\u200B", $"```Defensive Stats```", false)
+                    .AddField("Tac - Tac Att", pStat.Tackling, true)
+                    .AddField("Tac/Game", pStat.TacklesPerGame, true)
+                    .AddField("Tac %", pStat.TacklePercent, true)
+                    .AddField("PossW-PossL", pStat.Poss, true)
+                    .AddField("Int-Blk", pStat.Wall, true)
+
+                    .AddField("\u200B", $"```Discipline```", false)
+                    .AddField("YC-RC", pStat.Discipline, true);
+                embed = builder.Build();
+            }
+            return embed;
         }
     }
 }
