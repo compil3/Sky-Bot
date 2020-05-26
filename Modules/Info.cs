ce75ff6a0b6f3a458f2f6ca35afcfa6a9d037227
compil3 <luca.tenuta@gmail.com>
2020-05-26 03:05:05 -0400

    added info command

---
+++ Modules/Info.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using LGFA.Extensions;
+using LGFA.Properties;
+
+namespace LGFA.Modules
+{
+    [RequireContext(ContextType.Guild)]
+    [RequireUserPermission(ChannelPermission.SendMessages)]
+    public class Info : ModuleBase
+    {
+        [Command("info")]
+        [Summary("Gets the league general information.")]
+        public async Task GetInfo(string system)
+        {
+            Embed embed = null;
+            var systemIcon = "";
+            List<LeagueProperties> leagueInfo = null;
+
+            switch (system)
+            {
+                case "psn":
+                    systemIcon =
+                        "https://cdn.discordapp.com/attachments/689119430021873737/711030693743820800/220px-PlayStation_logo.svg.jpg";
+                    break;
+                case "xbox":
+                    systemIcon = "https://cdn.discordapp.com/attachments/689119430021873737/711030386775293962/120px-Xbox_one_logo.svg.jpg";
+                    break;
+            }
+            leagueInfo = LeagueInfo.GetSeason(system);
+            foreach (var info in leagueInfo)
+            {
+                var builder = new EmbedBuilder()
+                    .WithAuthor(author =>
+                    {
+                        author
+                            .WithName($"LGFA {system.ToUpper()} Info")
+                            .WithIconUrl(systemIcon);
+                    })
+                    .AddField("Season", info.Season, true)
+                    .AddField("Season Type", info.SeasonType, true)
+                    .AddField("Week", info.Week, true)
+                    .AddField("User", info.User, true)
+                    .AddField("Completed/Games", info.Games, true);
+                embed = builder.Build();
+            }
+
+            await Context.Channel.SendMessageAsync($"``[Information provided by LGFA]``", embed: embed)
+                .ConfigureAwait(false);
+        }
+    }
+}
