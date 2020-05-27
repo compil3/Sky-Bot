63f65fdfe66d2f7b65f16feaa429f8799d5f9457
compil3 <luca.tenuta@gmail.com>
2020-05-27 12:54:04 -0400

    updated scheduler

--- Schedule/Manager.cs
+++ Schedule/Manager.cs
@@ -9,15 +9,17 @@ using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
 using FluentScheduler;
+using LGFA.Modules.News;
 using Serilog;
 
 namespace LGFA.Schedule
 {
     public class Manager : ModuleBase
     {
-        public static Task Manage(IMessageChannel chnl)
+        public static Task Manage(IMessageChannel chnl, IMessageChannel news)
         {
-
+            JobManager.Initialize(new Trades(news));
+            JobManager.Initialize(new WaiverNews(news));
             JobManager.Initialize(new WeekUpdate(chnl));
             Log.Logger.Information("Schedule Initialized.");
             return Task.CompletedTask;
