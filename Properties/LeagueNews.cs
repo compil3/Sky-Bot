a213fd66fbf770c24b1e54b9f3e0cc5f42b5372a
compil3 <luca.tenuta@gmail.com>
2020-05-27 12:52:52 -0400

    added news props

---
+++ Properties/LeagueNews.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LGFA.Properties
+{
+    class LeagueNews
+    {
+        public class News
+        {
+            public int Id { get; set; }
+            public DateTime Date { get; set; }
+
+            public string TeamOneName { get; set; }
+            public string TeamOneIcon { get; set; }
+
+            public string TeamTwoName { get; set; }
+            public string TeamTwoIcon { get; set; }
+
+            public string NewsLineOne { get; set; }
+            public string NewsLineTwo { get; set; }
+
+            public string System { get; set; }
+        }
+
+        public class Waivers
+        {
+            public int Id { get; set; }
+            public DateTime Date { get; set; }
+            public string Line { get; set; }
+            public string System { get; set; }
+        }
+    }
+}
