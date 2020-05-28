9d88d8e16fa4a407e5db0c7a38972d7283a6075e
compil3 <luca.tenuta@gmail.com>
2020-05-28 00:38:22 -0400

    commented out logger and  cleaned up code.

--- Modules/News/Trades.cs
+++ Modules/News/Trades.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 using Discord;
-using Discord.Commands;
-using Discord.WebSocket;
 using FluentScheduler;
 using HtmlAgilityPack;
-using LGFA.Modules.News;
-using LGFA.Properties;
-using LiteDB;
-using Quartz;
-using Serilog;
 
 
 namespace LGFA.Modules.News
@@ -25,14 +16,14 @@ namespace LGFA.Modules.News
             {
                 var web = new HtmlWeb();
                 new HtmlWeb();
-                List<string> leagueList = new List<string>() {"53", "73"};
+                var leagueList = new List<string>() {"53", "73"};
 
                 foreach (var id in leagueList)
                 {
                     var feed = web.Load(
                         $"https://www.leaguegaming.com/forums/index.php?leaguegaming/league&action=league&page=team_news&leagueid=" + id + "&typeid=7");
-                    await News.Feed.RunTrades(feed, id, chnl);
-                    Log.Logger.Information("Trades ran.");
+                    await Feed.RunTrades(feed, id, chnl);
+                    //Log.Logger.Information("Trades ran.");
                 }
             });
             this.Schedule(trades).ToRunEvery(2).Seconds();
