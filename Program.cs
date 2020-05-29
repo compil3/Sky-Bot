c996535343f387ce46473c0c8c391d767b2785e5
compil3 <luca.tenuta@gmail.com>
2020-05-29 09:28:07 -0400

    cleaned up

--- Program.cs
+++ Program.cs
@@ -39,7 +39,6 @@ namespace LGFA
             Console.WriteLine("Sky Sports Bot v2.0");
             Console.ResetColor();
 
-
             await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("token"));
 
             await client.StartAsync();
@@ -55,9 +54,8 @@ namespace LGFA
 
         public static Task Client_Ready()
         {
-            var id = Convert.ToUInt64(Environment.GetEnvironmentVariable("update_log_channel"));
             var newsChn = client.GetChannel(Convert.ToUInt64(Environment.GetEnvironmentVariable("news_channel"))) as IMessageChannel;
-            var chnl = client.GetChannel(id) as IMessageChannel;
+            var chnl = client.GetChannel(Convert.ToUInt64(Environment.GetEnvironmentVariable("update_log_channel"))) as IMessageChannel;
             Manager.Manage(chnl, newsChn);
             client.ReactionAdded += RoleHandler.OnRulesReaction;
             client.ReactionAdded += RoleHandler.OnSystemReaction;
