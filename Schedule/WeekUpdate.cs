8d3921e64721a3f85cc298b06dbc7f0e0df27b8c
compil3 <luca.tenuta@gmail.com>
2020-05-29 12:32:33 -0400

    changed condition.

--- Schedule/WeekUpdate.cs
+++ Schedule/WeekUpdate.cs
@@ -51,7 +51,7 @@ namespace LGFA.Schedule
 
                 try
                 {
-                    if (!RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                     {
                         foreach (var t in seasonCount)
                         {
