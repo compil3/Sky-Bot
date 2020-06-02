f446f2228052b6d1c90fa88359e20ce8e1175beb
compil3 <luca.tenuta@gmail.com>
2020-06-02 12:45:03 -0400

    fixed output

--- Modules/Helpers/TeamHelper.cs
+++ Modules/Helpers/TeamHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Discord;
 using LGFA.Engines;
+using LGFA.Extensions;
 
 namespace LGFA.Modules.Helpers
 {
@@ -12,6 +13,7 @@ namespace LGFA.Modules.Helpers
             var ranking = string.Join(Environment.NewLine, teamStandings);
             var points = string.Join(Environment.NewLine, teamPoints);
             var systemIcon = "";
+            EmbedBuilder builder = null;
             if (teamStandings == null)
             {
                 return NoTable(league);
@@ -23,26 +25,29 @@ namespace LGFA.Modules.Helpers
             else if (system == "XBOX" || system == "xbox")
                 systemIcon =
                     "https://cdn.discordapp.com/attachments/689119430021873737/711030386775293962/120px-Xbox_one_logo.svg.jpg";
-
-            var builder = new EmbedBuilder()
-                .WithAuthor(author =>
-                {
-                    author
-                        .WithName($"LGFA {system}")
-                        .WithIconUrl(systemIcon);
-                })
-                .WithTitle("Current Standings")
-                .WithUrl(leagueUrl)
-                .WithColor(new Color(0x26A20B))
-                .WithCurrentTimestamp()
-                .WithFooter(footer =>
-                {
-                    footer
-                        .WithText("leaguegaming.com")
-                        .WithIconUrl("https://www.leaguegaming.com/images/logo/logonew.png");
-                })
-                .AddField("Ranking", ranking, true)
-                .AddField("Points", points, true);
+            var lgInfo = LeagueInfo.GetSeason(league);
+            foreach (var info in lgInfo)
+            {
+                builder = new EmbedBuilder()
+                    .WithAuthor(author =>
+                    {
+                        author
+                            .WithName($"LGFA {system}")
+                            .WithIconUrl(systemIcon);
+                    })
+                    .WithTitle($"Current Standings - S{info.Season}")
+                    .WithUrl(leagueUrl)
+                    .WithColor(new Color(0x26A20B))
+                    .WithCurrentTimestamp()
+                    .WithFooter(footer =>
+                    {
+                        footer
+                            .WithText("leaguegaming.com")
+                            .WithIconUrl("https://www.leaguegaming.com/images/logo/logonew.png");
+                    })
+                    .AddField("Ranking", ranking, true)
+                    .AddField("Points", points, true);
+            }
             return builder.Build();
         }
 
@@ -50,7 +55,7 @@ namespace LGFA.Modules.Helpers
         {
             EmbedBuilder builder = null;
             builder = new EmbedBuilder()
-                .WithTitle("Couldn't find Standings Table for the current season.")
+                .WithTitle("Season has not started yet or could not be found.")
                 .WithFooter(footer =>
                 {
                     footer
