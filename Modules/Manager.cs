48aa4aef5b1068b689f2a360b9e002ae9c4165ef
compil3 <luca.tenuta@gmail.com>
2020-06-10 14:58:10 -0400

    fixed

--- Modules/Manager.cs
+++ Modules/Manager.cs
@@ -1,5 +1,13 @@
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
+using LGFA.Properties;
+using LiteDB;
 
 namespace Sky_Bot.Modules
 {
@@ -15,11 +23,55 @@ namespace Sky_Bot.Modules
 
             [Command("ab")]
             [Alias("addblock")]
-            public async Task AddBlock(string _name, [Optional]string _value) => await ReplyAsync("");
+            public async Task AddBlock(string _name, [Optional]string _value)
+            {
+                if (Context.User is SocketGuildUser gUser)
+                {
+                    if (gUser.Roles.Any(r => r.Name == "Manager"))
+                    {
+                        AddToBlock(_name, _value);
+                        await Context.User.SendMessageAsync("PAGINATED TRADEBLOCK EMBED");
+                    }
+                }
+                await ReplyAsync("");
+            }
+
+           
 
             [Command("rb")]
             [Alias("removeblock")]
-            public async Task RemoveBlock(string name) => await ReplyAsync("");
+            public async Task RemoveBlock(string name)
+            {
+                if (Context.User is SocketGuildUser gUser)
+                {
+                    if (gUser.Roles.Any(r => r.Name == "Manager"))
+                    {
+                        await Context.User.SendMessageAsync("REMOVE USER FROM TB");
+                    }
+                }
+                await ReplyAsync("");
+            }
+        }
+        private void AddToBlock(string name, string value)
+        {
+            var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            var dbFolder = "Database/";
+            var dbDir = Path.Combine(path, dbFolder);
+
+            using var tradeDb = new LiteDatabase($"Filename={dbDir}LGFA.db;connection=shared");
+            var block = tradeDb.GetCollection<TradeBlock.BlockProperties>("TradeBlock");
+
+            block.EnsureIndex(x => x.Name);
+
+            var result = block.Query()
+                .Where(x => x.Name.Contains(name))
+                .ToList();
+
+            foreach (var t in result)
+            {
+                
+            }
+            throw new System.NotImplementedException();
         }
 
     
---
+++ Properties/TradeBlock.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LGFA.Properties
+{
+    class TradeBlock
+    {
+        public class BlockProperties
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
+        }
+    }
+}
