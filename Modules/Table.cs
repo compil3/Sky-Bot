ae6c7bb98592f876d3795335a776a2c65b9a4e8c
compil3 <luca.tenuta@gmail.com>
2020-08-13 22:14:01 -0400

    cleaned up Table command.

--- Modules/Table.cs
+++ Modules/Table.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using LGFA.Engines;
+using LGFA.Engines.Current;
 using LGFA.Extensions;
 using LGFA.Modules.Helpers;
 using Serilog;
@@ -19,47 +20,13 @@ namespace LGFA.Modules
         public async Task Standings(string league)
         {
             EmbedBuilder embed = new EmbedBuilder();
-            var systemIcon = "";
-            var standingsUri =
-                "https://www.leaguegaming.com/forums/index.php?leaguegaming/league&action=league&page=standing&";
             var options = new RequestOptions {Timeout = 2};
             await Context.Message.DeleteAsync(options);
 
             if (Context.Channel.Id == Convert.ToUInt64(Environment.GetEnvironmentVariable("stats_channel")))
             {
-               
-                var (teamStandings, teamPoints, currentSeason, leagueUrl, system) = TeamStanding.GetStandings(league);
-                if (teamStandings == null)
-                {
-                    TeamHelper.NoTable(league);
-                    return;
-                }
-                if (league.Contains("psn"))
-                {
-                    systemIcon ="https://cdn.discordapp.com/attachments/689119430021873737/711030693743820800/220px-PlayStation_logo.svg.jpg";
-                    standingsUri = standingsUri + "&leagueid=73&seasonid=" + currentSeason;
-                }
-                else if (league.Contains("xbox"))
-                {
-                    systemIcon = "https://cdn.discordapp.com/attachments/689119430021873737/711030386775293962/120px-Xbox_one_logo.svg.jpg";
-                    standingsUri = standingsUri + "&leagueid=53&seasonid=" + currentSeason;
-
-                }
-                StandingBuilder(teamStandings, teamPoints, ref embed);
-
-                embed.Title = $"Current Standings - S{currentSeason}";
-                embed.WithUrl(standingsUri);
-                embed.Author = new EmbedAuthorBuilder()
-                {
-                    Name = $"[Table provided by Leaguegaming.com]\n League: {league.ToUpper()}",
-                    IconUrl = systemIcon
-                };
-                embed.Footer = new EmbedFooterBuilder()
-                {
-                    Text = "leaguegaming.com",
-                    IconUrl = "https://www.leaguegaming.com/images/logo/logonew.png"
-                };
-                await ReplyAsync("", embed: embed.Build());
+               TeamStanding.GetStandings(league, ref embed,"Standings");
+               await ReplyAsync("", embed: embed.Build());
             }
             else
             {
@@ -67,15 +34,6 @@ namespace LGFA.Modules
                     $"{Context.User.Mention} you are using the command in the wrong channel, try again in " +
                     $"{MentionUtils.MentionChannel(Convert.ToUInt64(Environment.GetEnvironmentVariable("stats_channel")))}");
             }
-        }
-
-        public void StandingBuilder(List<string> ranking, List<string> points, ref EmbedBuilder embed)
-        {
-            var _ranking = string.Join(Environment.NewLine, ranking);
-            var _points = string.Join(Environment.NewLine, points);
-
-            embed.AddField("Ranking", _ranking, true);
-            embed.AddField("Pts", _points, true);
         }
     }
 }
